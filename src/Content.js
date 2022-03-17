662a5d730edeac16e83341b949f73bac798bdb56
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-03-17 16:56:10 +0100

    notes[9]

--- src/Content.js
+++ src/Content.js
@@ -2,27 +2,9 @@ import React from "react";
 
 export default function Content(props) {
 
-
-
   const [noteChange, setNoteChange] = React.useState({
-    title: "", content: ""
+    title: '', content: ''
   })
-
-  console.log(noteChange)
-
-  React.useEffect(()=>{
-    setNoteChange(()=>{
-      return{
-        title: props.activeNote.getElementsByClassName("note-title")[0].textContent,
-        content: props.activeNote.getElementsByClassName("note-desc")[0].textContent
-      } 
-    })
-  },[props.activeNote])
-
-
-  props.activeNote.getElementsByClassName("note-title")[0].textContent = noteChange.title ? noteChange.title : "Title..."
-  props.activeNote.getElementsByClassName("note-desc")[0].textContent = noteChange.content ? noteChange.content : "Note body..."
-  
 
 
   function change(event){
@@ -40,8 +22,8 @@ export default function Content(props) {
     <div className="content">
 
         <div className="write-container">
-          <input className="write-title" name="title" placeholder='Title...' onChange={change} value={noteChange.title}></input>
-          <textarea className="write-content" name="content" placeholder='Note body...' onChange={change} value={noteChange.content}></textarea>
+          <input className="write-title" name="title" placeholder='Title...' onChange={change} value={noteChange.title} ></input>
+          <textarea className="write-content" name="content" placeholder='Note body...' onChange={change} value={noteChange.content} ></textarea>
         </div>
 
     </div>
